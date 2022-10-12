aa0b319597be59779fc55e35b000a095b52d0bdc
Alexander Buitrago <111058346+Jab1981@users.noreply.github.com>
2022-10-12 11:51:36 -0500

    Controllers

---
+++ back/server.js
@@ -0,0 +1,10 @@
+const app=require("./app")
+
+//Setear archico config
+const dotenv=require("dotenv");
+dotenv.config  ({path:'back/config/config.env'})
+
+//Llamammos al server
+const server=app.listen(process.env.PORT,()=>{
+    console.log(`Servidor iniciando en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
