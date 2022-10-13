b0c7238428aca1f70ccd0c0bd8132c2ea2cda157
Alexander Buitrago <111058346+Jab1981@users.noreply.github.com>
2022-10-13 00:47:15 -0500

    Configuracion MongoDB

--- back/app.js
+++ back/app.js
@@ -1,4 +1,12 @@
 const express=require("express");
 const app=express();
 
+app.use(express.json());
+
+// Importamos las rutas 
+const productos=require("./routes/products")
+
+//Sujeta a la ruta en el navegador
+app.use('/api',productos)
+
 module.exports=app
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,3 +1,9 @@
 const express=require("express")
 const router=express.Router();
+
+//Se trae la repuesta json desde el controlador (productsController) 
 const {getProducts}=require("../controller/productsController")
+
+//Establecemos la ruta que queremos ver el (getProducts)
+router.route('/productos').get(getProducts)
+module.exports=router;
