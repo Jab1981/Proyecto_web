a87cfddc2fa4efe05cbddb3f95b52d16cb04b942
Alexander Buitrago <111058346+Jab1981@users.noreply.github.com>
2022-10-19 11:07:04 -0500

    css-header-index

---
+++ front/proyecto_web/src/components/layout/Header.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react'
+
+const Header = () => {
+  return (
+    <Fragment>
+        <nav className='navbar row'>
+            <div className='col-12 col-md-3'>
+                <div className='navbar-brand'>
+                    <img src= "./images/logo192.png" alt="logo pulgarin"></img>
+                </div>
+            </div>
+
+            <div>
+                
+            </div>
+        </nav>
+
+    </Fragment>
+  )
+}
+
+export default Header
